cd0c5225c1c1a0b3812d7eaeed682a668331945c
Chandra <cgjangid@gmail.com>
2021-07-06 10:40:57 +0100

    updated the assertions

--- src/test/java/com/actoJava/qa/endToEndTests/EndToEndTests.java
+++ src/test/java/com/actoJava/qa/endToEndTests/EndToEndTests.java
@@ -21,32 +21,32 @@ public class EndToEndTests extends BaseTest {
         UserRegisterPage userRegisterPage = loginPage.navigateToRegisterPage();
 
         Reporter.log("======Verify that Register Page is displayed======", true);
-        Assert.assertTrue(userRegisterPage.isRegisterPageDisplayed(), "Register Page is displayed");
+        Assert.assertTrue(userRegisterPage.isRegisterPageDisplayed(), "Register Page is NOT displayed");
 
         if (userRegisterPage.isRegisterPageDisplayed()) {
             Reporter.log("======Register a User with test data======", true);
             userRegisterPage.registerUser("firstName", "lastName", "email@test.com", "password");
 
             Reporter.log("======Verify that Login Page is displayed======", true);
-            Assert.assertTrue(loginPage.isLoginPageDisplayed(), "Login Page is displayed");
+            Assert.assertTrue(loginPage.isLoginPageDisplayed(), "Login Page is NOT displayed");
 
             if (loginPage.isLoginPageDisplayed()) {
                 Reporter.log("======Login to application with the registered user======", true);
                 HomePage homePage = loginPage.loginToApplication("email@test.com", "password");
 
                 Reporter.log("======Verify that user is logged in and Home Page is displayed======", true);
-                Assert.assertTrue(homePage.isHomePageDisplayed(), "Home Page is displayed");
+                Assert.assertTrue(homePage.isHomePageDisplayed(), "Home Page is NOT displayed");
 
                 if (homePage.isHomePageDisplayed()) {
                     Reporter.log("======Verify that username is displayed after login======", true);
                     String loggedInUserName = homePage.getLoggedInUserName();
-                    Assert.assertEquals(loggedInUserName, "Leonard Harper");
+                    Assert.assertEquals(loggedInUserName, "Leonard Harper", "Logged-in user name is incorrect");
 
                     Reporter.log("======Logout from application======", true);
                     homePage.logoutFromApp();
 
                     Reporter.log("======Verify that user is successfully logged out======", true);
-                    Assert.assertTrue(loginPage.isLoginPageDisplayed(), "Logout Successful");
+                    Assert.assertTrue(loginPage.isLoginPageDisplayed(), "Logout is NOT successful");
                 }
             }
         }
